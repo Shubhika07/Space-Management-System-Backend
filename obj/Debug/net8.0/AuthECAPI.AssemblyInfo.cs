31b48718cf2eb3dcd10651249420e4dc8329f64f
Shubhika Kashyap Ojha <shubhi07628@gmail.com>
2024-12-08 10:43:44 +0530

    Changed Database

--- obj/Debug/net8.0/AuthECAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/AuthECAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthECAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb922577e2bec5138cea84a245d8bc3ae0b762bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthECAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthECAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
